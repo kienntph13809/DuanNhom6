5f720f3bef3b682e05e4317997353f828ac7bf61
LENOVO <daoanhduc2k2@gmail.com>
2021-12-11 17:09:34 +0700

    1

--- QLNOITHAT_NHOM6.sql
+++ QLNOITHAT_NHOM6.sql
@@ -46,7 +46,6 @@ SOLUONG INT,
 Macl int,
 DONGIA MONEY,
 MADM int,
-ANHSP NVARCHAR(50),
 MOTA NVARCHAR(100),
 TRANGTHAI BIT,
 SoSP int identity
@@ -54,6 +53,10 @@ CONSTRAINT PK_SANPHAM PRIMARY KEY (MASP),
 foreign key(MADM) references danhmuc,
 foreign key(MaCl) references chatlieu
 )
+select SANPHAM.MASP from SANPHAM join SUKIEN on SANPHAM.MASP=SUKIEN.MASP
+
+
+
 --TÀI KHO?N
 if OBJECT_ID('TAIKHOAN')is not null
 drop table TAIKHOAN
@@ -73,16 +76,19 @@ drop table SUKIEN
 CREATE TABLE SUKIEN(
 MASK varchar(20) ,
 TENSK NVARCHAR(50),
-UUDAI FLOAT,
 TGBATDAU DATE,
 TGKETTHIC DATE,
 LoaiSuKien bit default 1,
 TRANGTHAI BIT,
-PRIMARY KEY (MASK)
+MASP VARCHAR(15),
+PRIMARY KEY (MASK),
+foreign key(MASP) references SANPHAM
 )
 select * from SUKIEN where TRANGTHAI=1
 
 select * from SUKIEN
+delete SUKIEN where  MASK=?, TRANGTHAI =0
+UPDATE SUKIEN set TRANGTHAI =0 where MASK= ? and UUDAI =?
 
 insert SUKIEN (MASK,TENSK,UUDAI,TGBATDAU,TGKETTHIC) values(?,?,?,?,?)
 IF OBJECT_ID('VIEW_SK') IS NOT NULL
@@ -109,11 +115,10 @@ SELECT 	CASE
 			WHEN DONGIA BETWEEN 25000000 AND 3000000 THEN (DONGIA /100) *10
 			WHEN DONGIA BETWEEN 3000001 AND 5000000 THEN (DONGIA /100)* 15
 			ELSE (DONGIA /100)*20
-		END AS DUOCGIAM,
-IIF(125000*100/5>2400000,N'Được Giảm Giá 5% - 20%',N'Không Đủ Điều Kiện') AS DieuKien
+		END AS DUOCGIAM
 
 FROM SANPHAM
-SELECT * FROM SANPHAM
+
 
 
 
@@ -163,6 +168,7 @@ select * from danhmuc
 select * from chatlieu
 select * from HOADON
 select * from TAIKHOAN
+SELECT * FROM SUKIEN
 
 insert into KHACHHANG values ('kh15',N'Nguy?n Anh D?ng','0897999565','Hà N?i',1)
 insert into KHACHHANG values ('kh16',N'Phạm Anh Tú','0897999555','Hà N?i',1)
@@ -177,30 +183,59 @@ insert into  chatlieu values (N'Gỗ',1)
 insert into  chatlieu values (N'Nhôm',1)
 insert into  chatlieu values (N'Nhựa',1)
 
-insert into SANPHAM values ('SP01',N'Bàn trạm hình rồng',5,1,6500000,1,'Ban Hinh Rong.jpg',N'Bàn đẹp hình Rồng uốn lượn',1)
-insert into SANPHAM values ('SP02',N'Ghế trạm hình rồng',3,1,4500000,2,'Ghe Hinh Rong.jpg',N'Ghế đẹp hình Rồng uốn lượn',1)
-insert into SANPHAM values ('SP03',N'Gương trạm hình rồng',2,1,2500000,3,'Guong Hinh Rong.jpg',N'Gương đẹp hình Rồng uốn lượn',1)
+insert into SANPHAM values ('SP01',N'Bàn trạm hình rồng',5,1,6500000,1,N'Bàn đẹp hình Rồng uốn lượn',1)
+insert into SANPHAM values ('SP02',N'Ghế trạm hình rồng',3,1,4500000,2,N'Ghế đẹp hình Rồng uốn lượn',1)
+insert into SANPHAM values ('SP03',N'Gương trạm hình rồng',2,1,2500000,3,N'Gương đẹp hình Rồng uốn lượn',1)
+insert into SANPHAM values ('SP04',N'Bàn Làm Việc',2,1,5500000,3,N'Đẹp Chất lượng tốt',1)
+insert into SANPHAM values ('SP05',N'Bàn làm việc',2,1,4500000,3,N'Kệ sách đẹp hình Rồng uốn lượn',1)
+insert into SANPHAM values ('SP06',N'Két sắt',2,1,4500000,3,N'Kệ sách đẹp hình Rồng uốn lượn',1)
+insert into SANPHAM values ('SP07',N'Kệ sách',2,1,4500000,3,N'Kệ sách đẹp hình Rồng uốn lượn',1)
+insert into SANPHAM values ('SP08',N'Kệ sách',2,1,4500000,3,N'Kệ sách đẹp hình Rồng uốn lượn',1)
+insert into SANPHAM values ('SP09',N'Kệ sách',2,1,4500000,3,N'Kệ sách đẹp hình Rồng uốn lượn',1)
+insert into SANPHAM values ('SP010',N'Kệ sách',2,1,4500000,3,N'Kệ sách đẹp hình Rồng uốn lượn',1)
+insert into SANPHAM values ('SP011',N'Kệ sách',2,1,4500000,3,N'Kệ sách đẹp hình Rồng uốn lượn',1)
+insert into SANPHAM values ('SP012',N'Kệ sách',2,1,4500000,3,N'Kệ sách đẹp hình Rồng uốn lượn',1)
+insert into SANPHAM values ('SP013',N'Kệ sách',2,1,4500000,3,N'Kệ sách đẹp hình Rồng uốn lượn',1)
+insert into SANPHAM values ('SP014',N'Kệ sách',2,1,4500000,3,N'Kệ sách đẹp hình Rồng uốn lượn',1)
+insert into SANPHAM values ('SP015',N'Kệ sách',2,1,4500000,3,N'Kệ sách đẹp hình Rồng uốn lượn',1)
+insert into SANPHAM values ('SP016',N'Kệ sách',2,1,4500000,3,N'Kệ sách đẹp hình Rồng uốn lượn',1)
+insert into SANPHAM values ('SP017',N'Kệ sách',2,1,4500000,3,N'Kệ sách đẹp hình Rồng uốn lượn',1)
+insert into SANPHAM values ('SP018',N'Kệ sách',2,1,4500000,3,N'Kệ sách đẹp hình Rồng uốn lượn',1)
+insert into SANPHAM values ('SP019',N'Kệ sách',2,1,4500000,3,N'Kệ sách đẹp hình Rồng uốn lượn',1)
+insert into SANPHAM values ('SP020',N'Kệ sách',2,1,4500000,3,N'Kệ sách đẹp hình Rồng uốn lượn',1)
+insert into SANPHAM values ('SP021',N'Kệ sách',2,1,4500000,3,N'Kệ sách đẹp hình Rồng uốn lượn',1)
+insert into SANPHAM values ('SP022',N'Kệ sách',2,1,4500000,3,N'Kệ sách đẹp hình Rồng uốn lượn',1)
 
 insert into TAIKHOAN values ('adminkien','123',N'Nguyễn Trung Kiên',0,1)
 insert into TAIKHOAN values ('admintu','123',N'Phạm Anh Tú',0,1)
 insert into TAIKHOAN values ('adminduc','123',N'Đào Văn Đức',1,1)
 
-insert into SUKIEN values ('1',N'KHUYẾN MÃI',20,GETDATE(),GETDATE()+5,1,1)
-insert into SUKIEN values ('2',N'KHUYẾN MÃI 30%',30,GETDATE(),GETDATE()+7,1,1)
-insert into SUKIEN values ('3',N'KHUYẾN MÃI 40%',40,GETDATE(),GETDATE()+10,1,1)
-insert into SUKIEN values ('4',N'KHUYẾN MÃI 40%',10,GETDATE(),GETDATE()+3,1,0)
-insert into SUKIEN values ('5',N'KHUYẾN MÃI 40%',13,GETDATE(),GETDATE()+4,1,1)
+insert into SUKIEN values ('SK01',N'KHUYẾN MÃI',GETDATE(),GETDATE()+5,1,1,'SP01')
+insert into SUKIEN values ('SK02',N'KHUYẾN MÃI 30%',GETDATE(),GETDATE()+7,1,1,'SP02')
+insert into SUKIEN values ('SK03',N'KHUYẾN MÃI 40%',GETDATE(),GETDATE()+10,1,1,'SP03')
+insert into SUKIEN values ('SK04',N'KHUYẾN MÃI 40%',GETDATE(),GETDATE()+3,1,0,'SP04')
+insert into SUKIEN values ('SK05',N'KHUYẾN MÃI 40%',GETDATE(),GETDATE()+4,1,1,'SP05')
+update SUKIEN SET TENSK =?, UUDAI =? WHERE MASK=?
 
 select SANPHAM.MASP,tensp,soluong,chatlieu.Macl,tendm,dongia,anhsp,mota
 from danhmuc join SANPHAM on danhmuc.madm =  SANPHAM.MADM
 join chatlieu on SANPHAM.Macl = chatlieu.Macl
  where tendm = 'Gương'
  insert into HOADON(MAHD,MASP,MAKH,TENTK,MASK,NGAYLAP,UUDAI,TONGTIENTT,TRANGTHAI,ghichu) values ('HD01','sp01','KH15','admintu',NULL,'2021-01-01',NULL,6500000,1,null)
-  insert into HOADON(MAHD,MASP,MAKH,TENTK,MASK,NGAYLAP,UUDAI,TONGTIENTT,TRANGTHAI,ghichu) values ('HD02','sp02','KH15','adminduc',NULL,'2021-01-01',NULL,6500000,1,null)
+  insert into HOADON(MAHD,MASP,MAKH,TENTK,MASK,NGAYLAP,UUDAI,TONGTIENTT,TRANGTHAI,ghichu) values ('HD02','sp02','KH16','admintu',NULL,'2021-01-01',NULL,3500000,1,null)
+  insert into HOADON(MAHD,MASP,MAKH,TENTK,MASK,NGAYLAP,UUDAI,TONGTIENTT,TRANGTHAI,ghichu) values ('HD03','sp03','KH17','adminduc',NULL,'2021-01-01',NULL,7500000,1,null)
+  insert into HOADON(MAHD,MASP,MAKH,TENTK,MASK,NGAYLAP,UUDAI,TONGTIENTT,TRANGTHAI,ghichu) values ('HD04','sp04','KH15','adminduc',NULL,'2021-01-01',NULL,6500000,1,null)
+
+
+
+
     insert into HOADON(MAHD,MASP,MAKH,TENTK,MASK,UUDAI,TONGTIENTT,TRANGTHAI,ghichu) values (?,?,?,?,?,?,?,?,?,?)
 	  insert into HOADON(MAHD,MASP,MAKH,TENTK,MASK,NGAYLAP,UUDAI,TONGTIENTT,TRANGTHAI,ghichu) values ('HD03','sp03','KH15','adminduc',NULL,NULL,6500000,1,null)
 
   insert into CTHOADON values('HD01','SP01',1,6500000,6500000,1)
+    insert into CTHOADON values('HD02','SP02',1,6500000,6500000,1)
+	  insert into CTHOADON values('HD03','SP03',1,6500000,6500000,1)
+
 
   SELECT *
             FROM HOADON JOIN TAIKHOAN ON HOADON.TENTK = TAIKHOAN.TENTK
